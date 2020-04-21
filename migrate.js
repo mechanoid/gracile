a9ffbc2d65045cf8f1c5e2913b8cd1c5f04cdbbd
Falk Hoppe <falkhoppe81+git@gmail.com>
2020-04-21 08:46:02 +0200

    walk through migrations and call them

--- migrate.js
+++ migrate.js
@@ -1,1 +1,13 @@
-export default async (options = { dir: './migrations' }) => {}
+import path from 'path'
+import fs from 'fs-extra'
+
+export default async (options = { dir: './migrations' }) => {
+  const dir = path.resolve(process.cwd(), options.dir)
+  await fs.ensureDir(dir)
+
+  const fileNames = await fs.readdir(dir)
+  const filePaths = fileNames.map(n => path.join(dir, n))
+  const migrations = await Promise.all(filePaths.map(f => import(f)))
+
+  migrations.forEach(m => m.up())
+}
