05385b3e565c8460239c8843c6f47e0a76d8b838
Falk Hoppe <falkhoppe81+git@gmail.com>
2020-04-21 08:02:09 +0200

    add simple file output for create

--- create.js
+++ create.js
@@ -1,5 +1,21 @@
 import path from 'path'
+import fs from 'fs-extra'
+
+const migrationFileName = migrationName => `${Date.now()}-${migrationName}.js`
+
 export default async (migrationName, options = { dir: './fsd' }) => {
   const dir = path.resolve(process.cwd(), options.dir)
-  console.log(migrationName, dir)
+  const fileName = migrationFileName(migrationName)
+  const filePath = path.resolve(dir, fileName)
+  const exists = await fs.pathExists(filePath)
+
+  await fs.ensureDir(dir)
+
+  if (exists) {
+    // should not ever happen, due to the timestamp, but who knows
+    throw new Error('migration file already exists')
+  }
+
+  await fs.outputFile(filePath, 'Hello World')
+  return `${filePath} has been successfully created`
 }
